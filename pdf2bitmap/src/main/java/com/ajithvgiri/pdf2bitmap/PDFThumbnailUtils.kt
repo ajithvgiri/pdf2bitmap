0ed71a50d389388864a9160252d195e38d736695
ajithvgiri <mail.ajithvgiri@gmail.com>
2019-03-07 18:42:26 +0530

    min api 21 to 16 commit

--- pdf2bitmap/src/main/java/com/ajithvgiri/pdf2bitmap/PDFThumbnailUtils.kt
+++ pdf2bitmap/src/main/java/com/ajithvgiri/pdf2bitmap/PDFThumbnailUtils.kt
@@ -4,15 +4,20 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.pdf.PdfRenderer
 import android.net.Uri
+import android.os.Build
 
 object PDFThumbnailUtils {
     fun convertPDFtoBitmap(context: Context, uri: Uri, pageNumber: Int): Bitmap? {
         val parcelFileDescriptor = context.contentResolver.openFileDescriptor(uri, "r")
-        val pdfRenderer = PdfRenderer(parcelFileDescriptor)
-        val currentPage = pdfRenderer.openPage(pageNumber)
-        val bitmap = Bitmap.createBitmap(currentPage.width, currentPage.height, Bitmap.Config.ARGB_8888)
-        currentPage.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
-        // Here, we render the page onto the Bitmap.
-        return bitmap
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val pdfRenderer = PdfRenderer(parcelFileDescriptor)
+            val currentPage = pdfRenderer.openPage(pageNumber)
+            val bitmap = Bitmap.createBitmap(currentPage.width, currentPage.height, Bitmap.Config.ARGB_8888)
+            currentPage.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
+            // Here, we render the page onto the Bitmap.
+            return bitmap
+        } else {
+            TODO("VERSION.SDK_INT < LOLLIPOP")
+        }
     }
 }
