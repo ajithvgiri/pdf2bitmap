28bacdeed70c472e5d4e87dd688c85a6c95be1d4
ajithvgiri <mail.ajithvgiri@gmail.com>
2019-03-08 01:03:20 +0530

    implemented pdfium for lower version support

--- pdf2bitmap/src/main/java/com/ajithvgiri/pdf2bitmap/PDFThumbnailUtils.kt
+++ pdf2bitmap/src/main/java/com/ajithvgiri/pdf2bitmap/PDFThumbnailUtils.kt
@@ -5,6 +5,9 @@ import android.graphics.Bitmap
 import android.graphics.pdf.PdfRenderer
 import android.net.Uri
 import android.os.Build
+import com.shockwave.pdfium.PdfiumCore
+import java.io.IOException
+
 
 object PDFThumbnailUtils {
     fun convertPDFtoBitmap(context: Context, uri: Uri, pageNumber: Int): Bitmap? {
@@ -19,5 +22,31 @@ object PDFThumbnailUtils {
         } else {
             TODO("VERSION.SDK_INT < LOLLIPOP")
         }
+    }
+
+
+    fun convertPDFtoBitmap(context: Context, uri: Uri): Bitmap? {
+        val pdfiumCore = PdfiumCore(context)
+        try {
+            val parcelFileDescriptor = context.contentResolver.openFileDescriptor(uri, "r")
+            if (parcelFileDescriptor != null) {
+                val pdfDocument = pdfiumCore.newDocument(parcelFileDescriptor)
+                pdfiumCore.openPage(pdfDocument, 0)
+                val width = pdfiumCore.getPageWidthPoint(pdfDocument, 0)
+                val height = pdfiumCore.getPageHeightPoint(pdfDocument, 0)
+                // ARGB_8888 - best quality, high memory usage, higher possibility of OutOfMemoryError
+                // RGB_565 - little worse quality, twice less memory usage
+                val bitmap = Bitmap.createBitmap(
+                    width, height,
+                    Bitmap.Config.RGB_565
+                )
+                pdfiumCore.renderPageBitmap(pdfDocument, bitmap, 0, 0, 0, width, height)
+                pdfiumCore.closeDocument(pdfDocument) // important!
+                return bitmap
+            }
+        } catch (ex: IOException) {
+            ex.printStackTrace()
+        }
+        return null
     }
 }
